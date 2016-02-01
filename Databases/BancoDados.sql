b157ee930e83b22ecaf92d52956f4d00eeaf993f
Caio Amaral Corrêa <caiobep.dev@gmail.com>
2016-02-01 18:06:52 -0200

    Adaptação do Banco
    Adaptação do Banco de Dados p/ Tela de Login

--- Databases/BancoDados.sql
+++ Databases/BancoDados.sql
@@ -7,6 +7,21 @@ DROP DATABASE IF EXISTs `amaral_guincho`;
 CREATE DATABASE `amaral_guincho`;
 
 USE `amaral_guincho`;
+CREATE TABLE user(
+  id_usr INT NOT NULL AUTO_INCREMENT,
+  login_usr VARCHAR(30) NOT NULL,
+  email_usr CHAR(50) NULL,
+  pwd_usr CHAR(50) NOT NULL,
+  type_usr ENUM('adm','staff','lowStaff','usr') NOT NULL,
+  nome_usr VARCHAR(50) NOT NULL,
+  sx_usr ENUM('M','F') NOT NULL,
+  birth_usr DATE NOT NULL,
+  cpf_usr CHAR(11) NULL,
+  cep_usr CHAR(11) NULL,
+  mobile_usr CHAR(12) NOT NULL,
+
+  PRIMARY KEY (id_usr)
+);
 
 CREATE TABLE frota(
    id_frota TINYINT NOT NULL AUTO_INCREMENT,
@@ -106,8 +121,25 @@ CREATE TABLE seguro(
   PRIMARY KEY (id_seguro)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE seguro_cli(
+  id_cli INT NOT NULL AUTO_INCREMENT,
+  id_usr INT NOT NULL,
+  id_seguro TINYINT NOT NULL,
+  tipo_seguro VARCHAR(20) NOT NULL,
+  assistente VARCHAR(20) NOT NULL,
+
+  PRIMARY KEY(id_cli),
+
+  FOREIGN KEY(id_usr)
+          REFERENCES user(id_usr),
+
+  FOREIGN KEY(id_seguro)
+          REFERENCES seguro(id_seguro)
+);
+
 CREATE TABLE habilitacao(
    id_habilitacao  SMALLINT NOT NULL AUTO_INCREMENT,
+   id_motorista INT NOT NULL,
    nome_habilitacao  VARCHAR(45) NOT NULL,
    identidade  INT NOT NULL,
    numero_registro  INT(12) NOT NULL,
@@ -116,42 +148,15 @@ CREATE TABLE habilitacao(
    local_gerado  VARCHAR(30) NOT NULL,
    emissao  DATE NOT NULL,
 
-  PRIMARY KEY (id_habilitacao)
+  PRIMARY KEY (id_habilitacao),
+
+  FOREIGN KEY(id_motorista)
+          REFERENCES user(id_usr)
 );
-
-CREATE TABLE funcionario(
-   id_funcionario  SMALLINT NOT NULL AUTO_INCREMENT,
-   id_habilitacao SMALLINT NULL,
-   nome_funcionario  VARCHAR(20) NOT NULL,
-   categoria_funcionario  ENUM('adm','staff','thirdy','none'),
-   end_funcionario  VARCHAR(20) NOT NULL,
-   cel_funcionario  CHAR(11) NOT NULL,
-   fixo_funcionario  CHAR(10) NULL,
-   data_contratacao_funcionario DATE NOT NULL,
-
-  PRIMARY KEY (id_funcionario),
-
-  FOREIGN KEY (id_habilitacao)
-    REFERENCES habilitacao (id_habilitacao)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE cliente(
-   id_cliente  INT NOT NULL AUTO_INCREMENT,
-   id_seguro  TINYINT NOT NULL,
-   nome_cliente  VARCHAR(30) NOT NULL,
-   rg_cliente  CHAR(11) NOT NULL,
-   cpf_cliente  CHAR(11) NOT NULL,
-   end_cliente  VARCHAR(30) NOT NULL,
-
-  PRIMARY KEY (id_cliente),
-  CONSTRAINT UNIQUE (cpf_cliente),
-  FOREIGN KEY (id_seguro)
-    REFERENCES seguro (id_seguro)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE veiculo(
    id_veiculo  INT NOT NULL AUTO_INCREMENT,
-   id_cliente INT NOT NULL,
+   id_usr INT NOT NULL,
    marca_veiculo  VARCHAR(30) NOT NULL,
    modelo_veiculo  VARCHAR(30) NOT NULL,
    ano_veiculo  DATE NOT NULL,
@@ -159,22 +164,8 @@ CREATE TABLE veiculo(
    cor_veiculo  VARCHAR(10) NOT NULL,
 
   PRIMARY KEY (id_veiculo),
-  FOREIGN KEY (id_cliente)
-    REFERENCES cliente (id_cliente)
-);
-
-CREATE TABLE user(
-  id_user INT NOT NULL AUTO_INCREMENT,
-  login_user VARCHAR(30) NOT NULL,
-  password_user VARCHAR(30) NOT NULL,
-  id_funcionario SMALLINT NULL,
-  id_cliente INT NULL,
-
-  PRIMARY KEY (id_user),
-  FOREIGN KEY (id_funcionario)
-    REFERENCES funcionario (id_funcionario),
-  FOREIGN KEY (id_cliente)
-    REFERENCES cliente(id_cliente)
+  FOREIGN KEY (id_usr)
+    REFERENCES user (id_usr)
 );
 
 CREATE TABLE ordem_de_servico(
@@ -189,29 +180,30 @@ CREATE TABLE ordem_de_servico(
    agendamento DATE NULL,
    numero_sinistro INT NOT NULL,
    local_entrega VARCHAR(30) NOT NULL,
-   id_motorista SMALLINT NOT NULL,
+   id_motorista INT NOT NULL,
   #MOTORISTA
 
 
   PRIMARY KEY (id_oe),
   FOREIGN KEY (id_seguro)
     REFERENCES seguro (id_seguro),
+
   FOREIGN KEY (id_cliente)
-    REFERENCES cliente (id_cliente),
+    REFERENCES user (id_usr),
   #Motorista
   FOREIGN KEY (id_motorista)
-    REFERENCES funcionario (id_funcionario)
+    REFERENCES user (id_usr)
 );
 
 CREATE TABLE session(
    id_session INT NOT NULL AUTO_INCREMENT,
-   id_funcionario SMALLINT NOT NULL,
+   id_funcionario INT NOT NULL,
    data_session TIMESTAMP NOT NULL,
    id_oe INT NOT NULL,
 
   PRIMARY KEY (id_session),
   FOREIGN KEY (id_funcionario)
-    REFERENCES funcionario (id_funcionario),
+    REFERENCES user (id_usr),
   FOREIGN KEY (id_oe)
     REFERENCES ordem_de_servico (id_oe)
 );
