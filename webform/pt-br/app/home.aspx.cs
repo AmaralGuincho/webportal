b459522364b9d891f79ce0d79517c7bbddc71ba0
Caio Amaral Corrêa <caiobep@me.com>
2016-05-31 23:32:15 -0300

    Added Motoristas Card Backend

--- webform/pt-br/app/home.aspx.cs
+++ webform/pt-br/app/home.aspx.cs
@@ -211,6 +211,45 @@ public partial class pt_br_app_home : System.Web.UI.Page
           nomeFunc2.InnerHtml = "--";
           nomeFunc3.InnerHtml = "--";
         }
+
+        //Importing Enployes from sql statement
+          DataView recentMotoristas = (DataView)lattestMotoristas.Select(DataSourceSelectArguments.Empty);
+          if (recentMotoristas.Table.Rows.Count > 0) {
+            //Checking wheather user have a profile image or not
+            if (recentMotoristas.Table.Rows[0]["img_func"].ToString() == String.Empty) {
+              imgMot1.Attributes["src"] = "../images/profiles/generic.png";
+            }else{
+              imgMot1.Attributes["src"] = recentMotoristas.Table.Rows[0]["img_func"].ToString();
+            }
+            nomeMot1.InnerHtml = recentMotoristas.Table.Rows[0]["nome_func"].ToString() + " " + recentMotoristas.Table.Rows[0]["sobrenome_func"].ToString();
+            //Checking 2nd Row
+            if(recentMotoristas.Table.Rows.Count > 1){
+              if (recentMotoristas.Table.Rows[1]["img_func"].ToString() == String.Empty) {
+                imgMot2.Attributes["src"] = "../images/profiles/generic.png";
+              }else{
+                imgMot2.Attributes["src"] = recentMotoristas.Table.Rows[1]["img_func"].ToString();
+              }
+              nomeMot2.InnerHtml = recentMotoristas.Table.Rows[1]["nome_func"].ToString() + " " + recentMotoristas.Table.Rows[1]["sobrenome_func"].ToString();
+              //Checking 3rd Row
+              if(recentMotoristas.Table.Rows.Count > 2){
+                if (recentMotoristas.Table.Rows[2]["img_func"].ToString() == String.Empty) {
+                  imgMot3.Attributes["src"] = "../images/profiles/generic.png";
+                }else{
+                  imgMot3.Attributes["src"] = recentMotoristas.Table.Rows[2]["img_func"].ToString();
+                }
+                nomeMot3.InnerHtml = recentMotoristas.Table.Rows[2]["nome_func"].ToString() + " " + recentMotoristas.Table.Rows[2]["sobrenome_func"].ToString();
+              }else{
+                nomeMot3.InnerHtml = "--";
+              }
+            }else{
+              nomeMot2.InnerHtml = "--";
+              nomeMot3.InnerHtml = "--";
+            }
+          }else{
+            nomeMot1.InnerHtml = "--";
+            nomeMot2.InnerHtml = "--";
+            nomeMot3.InnerHtml = "--";
+          }
       }
       catch(Exception ex){}
     }
