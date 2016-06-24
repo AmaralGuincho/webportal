49376fbc3966c54a07c9d910e4f1082781ed4836
Caio Amaral Corrêa <caiobep@me.com>
2016-06-24 16:32:09 -0300

    "Removed Segurohadler.js"

--- segurohandler.js
+++
@@ -1,7 +0,0 @@
-
-
-function prequica(camposSeguro) {
-  for (var i = 0; i < camposSeguro.length; i++) {
-    console.log('<asp:ControlParameter Name="+camposSeguro[i]+" ControlID="+camposSeguro[i]+" Property="Text"/>'
-  }
-}
