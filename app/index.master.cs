413b97f44644a2453462f6d5a1bdfca47b6e9596
Caio Amaral Corrêa <caiobep@me.com>
2016-10-02 11:27:58 -0300

    Mensagem de Boas Vindas

--- app/index.master.cs
+++ app/index.master.cs
@@ -9,42 +9,76 @@ using System.Data;
 
 public partial class app_index : System.Web.UI.MasterPage
 {
-  protected void Page_Load(object sender, EventArgs e)
+  public string getUserInfo(string info){
+    //Creating a new DataView
+    // Putting MySql's information inside our `Usuario` DataView
+    DataView user = (DataView)getUsuario.Select(DataSourceSelectArguments.Empty);
+
+    // User data to be shown
+   string userId = user.Table.Rows[0]["id_func"].ToString();
+   string userName = user.Table.Rows[0]["nome_func"].ToString();
+   string userLastName = user.Table.Rows[0]["sobrenome_func"].ToString();
+   string userFullName = userName + " " + userLastName;
+   string userMail = user.Table.Rows[0]["email_func"].ToString();
+   string image = user.Table.Rows[0]["img_func"].ToString();
+
+   Session["userFullName"] = userFullName;
+
+   switch (info){
+      case "name":
+        return userName;
+        break;
+      case "id":
+        return userId;
+        break;
+      case "mail":
+        return userMail;
+        break;
+      case "last":
+        return userLastName;
+        break;
+      case "fullName":
+        return userFullName;
+        break;
+      case "image":
+        return image;
+        break;
+      default:
+        return null;
+        break;
+   }
+  }
+
+  public void Page_Load(object sender, EventArgs e)
   {
     if(!IsPostBack){
-      try{
+      // try{
         //Verifying if user have a elegible log
         if(Session["log"] == null){
           //User does not have a elegible log
           Response.Redirect("~/index.html");
         }else{
           //User is Authenticated!
-          //Creating a new DataView
-          DataView user;
-          // Putting MySql's information inside our `Usuario` DataView
-          user = (DataView)getUsuario.Select(DataSourceSelectArguments.Empty);
+          string name = getUserInfo("name");
+          string mail = getUserInfo("mail");
+          string image = getUserInfo("image");
+          string genericImage = "../images/profiles/generic.png";
 
-          // User data to be shown
-          String userName = user.Table.Rows[0]["nome_func"].ToString();
-          String userFullName = userName + " " + user.Table.Rows[0]["sobrenome_func"].ToString();
-          String userMail = user.Table.Rows[0]["email_func"].ToString();
 
           //Displaying user information on screen
-          nome.InnerHtml = userName.ToString();
-          email.InnerHtml = userMail.ToString();
-          // Greeting The user
-          Session["serverMessage"] = "Logado como" + userFullName;
+          nome.InnerHtml = name;
+          email.InnerHtml = mail;
 
           //Checking if user does have a profile photo
-          if(user.Table.Rows[0]["img_func"].ToString() == String.Empty){
+          if(image == string.Empty){
             //using a generic profile photo
-            profileImage.Attributes["src"] = "../images/profiles/generic.png";
+            profileImage.Attributes["src"] = genericImage;
           }else{
             //Using the profile photo if the user does have one
-            profileImage.Attributes["src"] = user.Table.Rows[0]["img_func"].ToString();
+            profileImage.Attributes["src"] = image;
           }
         }
-        }catch(Exception ex){}
+        // }catch(Exception ex){}
       }
     }
 
@@ -55,4 +89,5 @@ public partial class app_index : System.Web.UI.MasterPage
       // Loging out User
       Session["log"] = null;
     }
+
   }
